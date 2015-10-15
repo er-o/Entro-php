cc56da682f1c6a2824412ff267730621723548ce
Cédric Berland <zeptoline2@gmail.com>
2015-10-15 10:49:01 +0200

    [MAJ]implementation session

---
+++ src/init.php
@@ -0,0 +1,23 @@
+<?php
+	session_start();
+
+	require "Joueur.php";
+
+	if(isset($_GET["nom_j1"])) {
+		$_SESSION["j1"] = new Joueur($_GET["nom_j1"]);
+		echo($_GET["nom_j1"]);
+	} else {
+		$_SESSION["j1"] = new Joueur("j1");
+	}
+
+	if(isset($_GET["nom_j2"])) {
+		$_SESSION["j2"] = new Joueur($_GET["nom_j2"]);
+	} else {
+		$_SESSION["j2"] = new Joueur("j2");
+	}
+
+
+
+	//header('Location: Application.php');
+
+?>
---
+++ src/launcher.php
@@ -0,0 +1,30 @@
+<?php
+	session_start();
+
+
+	$_SESSION["plateau"] = null;
+
+
+	
+?>
+
+<html>
+	<head>
+		<title>Jeu de l'Entropie</title>
+		<meta charset="utf-8" />
+	</head>
+	
+	<body>
+	
+		<h1>Jeu de l'entropie</h1>
+	
+		<p>Bienvenue sur le jeu de l'entropie. Le but est simple : devenir bloqué le plus rapidement possible.</p>
+
+		<form action="init.php" method="GET">
+			<label>Nom du Joueur 1</label><input type="text" name="nom_j1" /><br/>
+			<label>Nom du Joueur 2</label><input type="text" name="nom_j2" /><br/>
+			<button onclick="lanceur">Jouer maintenant</button>
+		</form>
+
+	</body>
+</html
